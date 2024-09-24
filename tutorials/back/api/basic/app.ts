000fce160ebd96491e47d25729e97289b72606ec
Ian Limson <ian.limson@student.vinci.be>
2024-09-24 12:17:39 +0200

    first tuto

---
+++ tutorials/back/api/basic/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { ErrorRequestHandler } from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+import drinkRouter from "./routes/drinks";
+import filmRouter from "./routes/films";
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+  };
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+app.use("/drinks", drinkRouter);
+app.use("/films", filmRouter);
+
+export default app;

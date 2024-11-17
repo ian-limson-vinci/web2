0d1f89776ea326525105c84460a6648db89f10ca
Ian Limson <ian.limson@student.vinci.be>
2024-11-18 00:48:09 +0100

    new:ex2.5-events

--- exercises/2.5/src/ClickCounter.tsx
+++ exercises/2.5/src/ClickCounter.tsx
@@ -8,10 +8,25 @@ interface ClickCounterProps {
 const ClickCounter = ({ title, on10ClicksMessage } : ClickCounterProps) => {
   const [count, setCount] = useState(0);
 
+  const[mouseOn, setMouseOn] = useState(false);
+
+  const handleMouseOn = () => {
+    setMouseOn(true);
+  }
+
+  const handleMouseOff = () => {
+    setMouseOn(false);
+  }
+
   return (
     <div className="card">
       <h1>{title}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
+      {mouseOn ? <p>Please click on me now !</p> : null}
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        onMouseEnter={handleMouseOn}
+        onMouseLeave={handleMouseOff}
+      >
         count is {count}
       </button>
       <br />

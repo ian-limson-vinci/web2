5077fc5c560c2bb05da9a11dd6138c4b2123d84e
Ian Limson <ian.limson@student.vinci.be>
2024-11-20 20:20:27 +0100

    new:ex2.11 (ex raté donc reprise de solutions)

--- exercises/2.10-11-12/src/main.tsx
+++ exercises/2.10-11-12/src/main.tsx
@@ -1,33 +1,41 @@
-import './index.css'
-import { HomePage,CinemaPage, MovieListPage } from './App.tsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const router = createBrowserRouter ([
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import App from "./components/App";
+import CinemaPage from "./components/pages/CinemaPage";
+import MovieListPage from "./components/pages/MovieListPage";
+import AddMoviePage from "./components/pages/AddMoviePage";
+
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children : [
+    children: [
       {
         path: "",
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
-        path: "cinema",
-        element: <CinemaPage />
+        path: "cinemas",
+        element: <CinemaPage />,
       },
       {
-        path: "movieList",
-        element: <MovieListPage />
+        path: "movie-list",
+        element: <MovieListPage />,
       },
-    ]
-  }
-])
+      {
+        path: "add-movie",
+        element: <AddMoviePage />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

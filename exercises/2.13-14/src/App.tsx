82e2397496899c28cfca5c5567378c5a7dcd32f1
Ian Limson <ian.limson@student.vinci.be>
2024-11-25 19:17:29 +0100

    new:ex2.13

---
+++ exercises/2.13-14/src/App.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [jokeCategory, setJokeCategory] = useState("");
+  const [jokeText, setJokeText] = useState("");
+
+  useEffect(() => {
+    console.log("a");
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            `fetch error : ${response.status} : ${response.statusText}`
+          );
+        return response.json();
+      })
+      .then((data) => {
+        setJokeCategory(data.category)
+        setJokeText(data.joke)
+      })
+      .catch((err) => {
+        console.error("Error: ", err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="joke">
+        <h2>Category : {jokeCategory}</h2>
+        <p>{jokeText}</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
